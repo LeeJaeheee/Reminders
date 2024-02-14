0ec78f3d126dae40ab7c0f80b041b929e5f565a8
LeeJaeheee <sns3434@gmail.com>
2024-02-14 13:56:09 +0900

    Initial Commit

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by 이재희 on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
